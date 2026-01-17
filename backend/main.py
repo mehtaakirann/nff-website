8d40494a8c9dc841e4ab584c9927593b521fba5c
mehtaakirann <kiran.mehta@cumminscollege.edu.in>
2026-01-17 18:32:05 +0530

    save changes

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-from database import engine
+from database import engine, SessionLocal
 from models import Base, FilmSubmission, Registration
+from schemas import FilmCreate, RegisterCreate
 
 app = FastAPI(title="NFF API")
+
+# Create tables
 Base.metadata.create_all(bind=engine)
+
 
 @app.post("/submit-film")
 def submit_film(film: FilmCreate):
@@ -11,7 +15,9 @@ def submit_film(film: FilmCreate):
     new_film = FilmSubmission(**film.dict())
     db.add(new_film)
     db.commit()
+    db.close()
     return {"status": "Film saved"}
+
 
 @app.post("/register")
 def register_user(user: RegisterCreate):
@@ -19,4 +25,5 @@ def register_user(user: RegisterCreate):
     new_user = Registration(**user.dict())
     db.add(new_user)
     db.commit()
+    db.close()
     return {"status": "User registered"}
