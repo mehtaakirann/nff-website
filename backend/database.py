6bd42058b40a804d6bf96934ba6ebfe2181b4f68
mehtaakirann <kiran.mehta@cumminscollege.edu.i>
2026-01-17 17:50:17 +0530

    Fix SQLAlchemy models and other updates

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://username:password@localhost/nff_db"
+DATABASE_URL = "postgresql://postgres:kiranm5@localhost/nff_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
--- backend/models.py
+++ backend/models.py
@@ -3,19 +3,20 @@ from database import Base
 
 class FilmSubmission(Base):
     _tablename_ = "submissions"
-    id = Column(Integer, primary_key=True)
+
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     director = Column(String)
     age_group = Column(String)
     link = Column(String)
 
-from sqlalchemy import Column, Integer, String
-from database import Base
 
 class Registration(Base):
     _tablename_ = "registrations"
-    id = Column(Integer, primary_key=True)
+
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     email = Column(String)
     role = Column(String)
 
+    
